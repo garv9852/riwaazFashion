059afdb3800e4bc49a536729708ff5644017bf6a
Garv kapoor <Garvk@LAPTOP-DEDK2DPV.(none)>
2021-12-30 18:11:11 +0530

    v1

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
             <>
                 <div className="first-container">
                     <div className="logo-container">
-                        <Link to="/riwaazFashion/"><img src='https://firebasestorage.googleapis.com/v0/b/riwaaz-fashion.appspot.com/o/LOGO%2Flooo-removebg-preview.png?alt=media&token=2dac9df4-f9b4-4e62-b44b-1351953a5e5f' className="logo-image"></img></Link>
+                        <Link to="/riwaazFashion"><img src='https://firebasestorage.googleapis.com/v0/b/riwaaz-fashion.appspot.com/o/LOGO%2Flooo-removebg-preview.png?alt=media&token=2dac9df4-f9b4-4e62-b44b-1351953a5e5f' className="logo-image"></img></Link>
                         <div className='material-icons cart-icon'>shopping_cart</div>
                     </div>
                 </div>
