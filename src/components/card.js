fcaa3bbe2690ebb16d9eb4c6273576a999e7f182
Garv kapoor <Garvk@LAPTOP-DEDK2DPV.(none)>
2021-12-30 19:54:58 +0530

    v2

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ export default class card extends Component {
         return (
             <>
                 <div className='flashCard-container'>
-                    {/* <img src="/looo-removebg-preview.png" style={{"height":"100%","width":"100%"}}></img> */}
+                    <img src="https://firebasestorage.googleapis.com/v0/b/riwaaz-fashion.appspot.com/o/LOGO%2FbannerRiwaaz.png?alt=media&token=3a0babfe-89ee-43d6-87f4-d9d9e33b7d89" style={{"height":"100%","width":"100%","box-shadow": "0 0 5px 5px #f3f3f3"}}></img>
                 </div>   
             </>
         )
